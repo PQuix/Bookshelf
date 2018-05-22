cf476257a4e4eb85935c1c0a19b3ba87cceb7cb5
Pål <Pal.K.Johansen@gmail.com>
2018-05-22 12:20:45 +0200

    Add project files.

---
+++ Bookshelf/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Bookshelf.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Bookshelf.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ Bookshelf/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Bookshelf.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Bookshelf.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
